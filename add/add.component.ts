7df2ad96fa5cbb630aa4a0e5c88a7d7b4aa09a91
Leaguejason <chuunbyou@gmail.com>
2022-01-15 19:42:46 +0700

    up code index

---
+++ add/add.component.ts
@@ -0,0 +1,14 @@
+import {Component, ViewChild} from "@angular/core";
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector:'add',
+  templateUrl:'./add.component.html'
+})
+
+export class AddComponent{
+  @ViewChild("f") addForm!: NgForm;
+  onSubmit(){
+    console.log(this.addForm.value);
+  }
+}
